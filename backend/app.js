577ae2b12b547004637b0b454cbc4836ce7ad234
Nidoveralis <nidoveralis@gmail.com>
2022-11-16 14:10:14 +0400

    feat: cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const router = require('./routes/router');
@@ -16,22 +17,28 @@ const allowedCors = ['https://mesto.diakova.nomoredomains.icu',
 
 const app = express();
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const allowMethods = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Methods', allowMethods);
-    return res.end();
-  }
-  next();
-});
+app.use(cors({
+  origin: allowedCors,
+  credentials: true,
+  methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Authorization', 'Content-type', 'Accept'],
+}));
+//app.use((req, res, next) => {
+  //const { origin } = req.headers;
+  //const { method } = req;
+  //const requestHeaders = req.headers['access-control-request-headers'];
+  //const allowMethods = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  //if (allowedCors.includes(origin)) {
+    //res.header('Access-Control-Allow-Origin', origin);
+    //res.header('Access-Control-Allow-Credentials', 'true');
+ // }
+  //if (method === 'OPTIONS') {
+    //res.header('Access-Control-Allow-Headers', requestHeaders);
+    //res.header('Access-Control-Allow-Methods', allowMethods);
+    //return res.end();
+  //}
+  //next();
+//});
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса

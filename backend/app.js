1021a16dd6a5217ea3dd66ca4aee02111392a17f
Nidoveralis <nidoveralis@gmail.com>
2022-11-16 19:52:17 +0400

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const mongoose = require('mongoose');
@@ -14,7 +15,7 @@ const allowedCors = [
   'http://mesto.diakova.nomoredomains.icu',
   'https://mesto.app.diakova.nomoredomains.icu',
   'http://mesto.app.diakova.nomoredomains.icu',
-  'http://localhost:3000'
+  'http://localhost:3000',
 ];
 
 const app = express();
@@ -25,22 +26,39 @@ const app = express();
 // methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
 // allowedHeaders: ['Authorization', 'Content-type', 'Accept'],
 // }));
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const allowMethods = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if ('https://mesto.diakova.nomoredomains.icu/'.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Methods', allowMethods);
-    return res.end();
-  }
-  next();
-});
+// app.use((req, res, next) => {
+// const { origin } = req.headers;
+// const { method } = req;
+// const requestHeaders = req.headers['access-control-request-headers'];
+// const allowMethods = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// if (allowedCors.includes(origin)) {
+// res.header('Access-Control-Allow-Origin', origin);
+// res.header('Access-Control-Allow-Credentials', 'true');
+// }
+// if (method === 'OPTIONS') {
+/// res.header('Access-Control-Allow-Headers', requestHeaders);
+// res.header('Access-Control-Allow-Methods', allowMethods);
+// return res.end();
+// }
+// next();
+// });
+
+const options = {
+  origin: [
+    'https://mesto.diakova.nomoredomains.icu/',
+    'https://mesto.diakova.nomoredomains.icu',
+    'http://mesto.diakova.nomoredomains.icu',
+    'https://mesto.app.diakova.nomoredomains.icu',
+    'http://mesto.app.diakova.nomoredomains.icu',
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+app.use('*', cors(options));
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса

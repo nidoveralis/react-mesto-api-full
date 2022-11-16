603643264aecca743f8f87161ac2b46b8d8a82f6
Nidoveralis <nidoveralis@gmail.com>
2022-11-16 20:47:22 +0400

    fix: cookie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -104,7 +104,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials({ email, password })
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
-      return res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true })
+      return res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true, sameSite: 'None', secure: true })
         .send({
           name: user.name, about: user.about, avatar: user.avatar, email: user.email, token,
         });

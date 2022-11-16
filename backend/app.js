889c414a0d8e282503cfe1809ae8a990d22baf9e
Nidoveralis <nidoveralis@gmail.com>
2022-11-16 17:05:57 +0400

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,9 @@ const errorsHandler = require('./middlewares/errorsHandler');
 const { errorLogger, requestLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
-const allowedCors = ['https://mesto.diakova.nomoredomains.icu',
+const allowedCors = [
+  'https://mesto.diakova.nomoredomains.icu/',
+  'https://mesto.diakova.nomoredomains.icu',
   'http://mesto.diakova.nomoredomains.icu',
   'https://mesto.app.diakova.nomoredomains.icu',
   'http://mesto.app.diakova.nomoredomains.icu',

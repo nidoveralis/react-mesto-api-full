31ee1ac572b9b01308633bcd94202ce88652fc7d
Nidoveralis <nidoveralis@gmail.com>
2022-11-17 20:02:06 +0400

    fix

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { errorLogger, requestLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const allowedCors = [
   'https://mesto.diakova.nomoredomains.icu',
-  'http://mesto.diakova.nomoredomains.icu',
+  //'http://mesto.diakova.nomoredomains.icu',
   'https://mesto.app.diakova.nomoredomains.icu',
   //'http://mesto.app.diakova.nomoredomains.icu',
   'http://localhost:3000',

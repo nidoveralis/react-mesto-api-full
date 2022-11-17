478e027abe2d090491e05c27d2aef02841a8eaea
Nidoveralis <nidoveralis@gmail.com>
2022-11-17 23:41:38 +0400

    feat:catch

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -39,7 +39,8 @@ function App() {
     console.log('user')
     api.signIn(data).then((user)=>{ console.log(user)
       openMainComponent();
-    });
+    })
+    .catch(err=>console.log(err))
   };
   
   function removeUserToken() {

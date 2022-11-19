e07135fdcd90a2f9dac01f1025bbd3fa507a171d
Nidoveralis <nidoveralis@gmail.com>
2022-11-19 19:40:53 +0400

    fix: isLiked

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,8 @@ function Card({card, openCard, onCardLike, onCardDelete}) {
 console.log(card)
   const currentUser = React.useContext(CurrentUserContext);
   const isOwn = card.owner === currentUser._id;
-  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  //const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const isLiked = card.likes.includes(currentUser._id);
 
   const cardDeleteButtonClassName = (
     `element__delete  popup-button ${isOwn ? "element__delete_active" : ""}`

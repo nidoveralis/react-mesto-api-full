c07e04c7efc26d467226a56485a43e507c363014
Nidoveralis <nidoveralis@gmail.com>
2022-11-19 14:05:34 +0400

    fix

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -36,9 +36,9 @@ function App() {
   };
 
   function logIn(data) {
-    api.signIn(data).then((user)=>{ console.log(user)
+    api.signIn(data).then((user)=>{
       openMainComponent();
-      //api.checkToken(user).then(data=>{
+      api.checkToken(user).then(data=>{console.log(data); console.log('jjj')})
         //setUserData(data.data.email);
         //openMainComponent();})
     })

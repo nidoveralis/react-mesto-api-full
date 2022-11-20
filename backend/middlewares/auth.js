d620dfc498f5ee1e16d259b6971417b706b08f7d
Nidoveralis <nidoveralis@gmail.com>
2022-11-20 15:10:50 +0400

    fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -12,8 +12,8 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
-  } catch (err) {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+} catch (err) {
     next(new IncorrectImailOrPassword('Необходима авторизация.'));
     return;
   }

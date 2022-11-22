3c4fb618c7f2337ca0ea9d786a1494c6cf81c71d
Nidoveralis <nidoveralis@gmail.com>
2022-11-22 10:39:26 +0400

    fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -10,6 +10,7 @@ module.exports = (req, res, next) => {
   }
   let payload;
   try {
+    console.log(NODE_ENV)
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new IncorrectImailOrPassword('Необходима авторизация.'));

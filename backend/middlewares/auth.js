f24c7962ed0746cfe74f790001be86d342a8b2be
Nidoveralis <nidoveralis@gmail.com>
2022-11-22 16:02:24 +0400

    fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,14 +1,10 @@
 const jwt = require('jsonwebtoken');
+const { JWT_SECRET, NODE_ENV } = process.env;
 const IncorrectImailOrPassword = require('../errors/IncorrectImailOrPassword');
 
 module.exports = (req, res, next) => {
-  const { JWT_SECRET = 'dev-secret', NODE_ENV = 'production' } = process.env;
-
   const token = req.cookies.jwt;
-  console.log(NODE_ENV)
-      console.log(NODE_ENV === 'production')
-      console.log(JWT_SECRET)
-      console.log(process.env.NODE_ENV);
+
   if (!token) {
     next(new IncorrectImailOrPassword('Необходима авторизация.'));
     return;
